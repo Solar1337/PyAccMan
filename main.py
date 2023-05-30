9080dc75877e1003ee02ac6a36f5509f9e5da71d
Solar <43799685+Solar1337@users.noreply.github.com>
2023-05-30 19:13:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#PyAccMan
+#Solar
+
 import hashlib, os
 
 def start():
@@ -11,9 +14,9 @@ def delete():
     delete = input("Are you sure you want to delete your account. This action cannot be undone. [Y]es, [N]o")
     if delete == "Y":
         os.remove(user + ".json")
-        print("Operation completed successfully.")
+        print("Your account has been deleted successfully.")
     elif delete == "N":
-        print("Your account has not been deleted")
+        print("Your account has not been deleted.")
         start()
     else:
         print("Response not valid.")
@@ -22,15 +25,15 @@ def sign_up():
     print("Sign Up")
     user = input("Username: ")
     if os.path.isfile(user + ".json") == True:
-        print("USER ALREADY EXISTS")
+        print("This username is already taken.")
     else:
-        f = open(user + ".json", "w+")
-        f.write('{\n    "user": ' + '"' + user +  '",\n')
         pw = input("Password: ")
         hash = hashlib.md5(pw.encode())
+        f = open(user + ".json", "w+")
+        f.write('{\n    "user": ' + '"' + user +  '",\n')
         f.write('    "hash": ' + '"' + hash.hexdigest() + '"\n}')
         f.close()
-        print("USER CREATED")
+        print("User '" + user + "' has been created successfully.")
 
 def sign_in():
     print("Sign In")
@@ -42,16 +45,17 @@ def sign_in():
         hash = hashlib.md5(pw.encode())
         if '"hash": ' + '"' +  hash.hexdigest() + '"' in f.read():
             f.close()
-            print("ACCESS GRANTED")
+            print("You have signed in successfully.")
             start()
         else:
-            print("ACCESS DENIED")
+            print("The password you have entered is incorrect.")
     else:
-        print("USER DOES NOT EXIST")
+        print("This user does not exist.")
 
 pre_start = input("1. Sign In\n2. Sign Up\n")
 if pre_start == "1":
     sign_in()
 elif pre_start == "2":
     sign_up()
-
+else:
+    print("Response not valid.")
